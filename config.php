2420b21aabb7d86bdde81c8ee1d9925accf29dcc
chrappa <kubino321@gmail.com>
2023-05-11 16:40:42 +0200

    config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//https://site194.webte.fei.stuba.sk/phpmyadmin/index.php?route=/&route=%2F
+$hostname = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "sadasad";
+
+
+?>
