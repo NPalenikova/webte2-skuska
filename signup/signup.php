9b64257f896a3649b1ce7786afd34711ded5f7d1
Natalia Palenikova <xpalenikovan@stuba.sk>
2023-05-15 11:57:17 +0200

    data received by server

--- signup/signup.php
+++ signup/signup.php
@@ -3,72 +3,27 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('config.php');
+require_once('../config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errmsg = "";
 
     $name = $_POST['name'];
     $surname = $_POST['surname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $password2 = $_POST['repeatPassword'];
     $userType = $_POST["userType"];
 
-    if(checkEmpty)
+    var_dump($_POST);
 
-    
-    // Validacia mailu
-    if (!checkStuba($email)) {
-        header('Location: signup.html');
-        exit();
+    if(checkEmpty($name) || checkEmpty($surname) || checkEmpty($email) || checkEmpty($password) || checkEmpty($password2) || checkEmpty($userType)){
+        $errmsg .= "<p>Data input was not correct.</p>";
     }
 
-    // Kontrola pouzivatela
-    if (userExist($pdo, $email, $userType) === true) {
-        header('Location: login.php');
-    }
-
-
-    // TODO: Validacia hesla
-    // TODO: Validacia mena, priezviska
-
-    if (empty($errmsg)) {
-        $sql = "INSERT INTO users (fullname, login, email, password, 2fa_code) VALUES (:fullname, :login, :email, :password, :2fa_code)";
-
-        $fullname = $_POST['firstname'] . ' ' . $_POST['lastname'];
-        $email = $_POST['email'];
-        $login = $_POST['login'];
-        $hashed_password = password_hash($_POST['password'], PASSWORD_ARGON2ID);
-
-        // 2FA pomocou PHPGangsta kniznice: https://github.com/PHPGangsta/GoogleAuthenticator
-        $g2fa = new PHPGangsta_GoogleAuthenticator();
-        $user_secret = $g2fa->createSecret();
-        $codeURL = $g2fa->getQRCodeGoogleUrl('Olympic Games', $user_secret);
-
-        // Bind parametrov do SQL
-        $stmt = $pdo->prepare($sql);
-
-        $stmt->bindParam(":fullname", $fullname, PDO::PARAM_STR);
-        $stmt->bindParam(":email", $email, PDO::PARAM_STR);
-        $stmt->bindParam(":login", $login, PDO::PARAM_STR);
-        $stmt->bindParam(":password", $hashed_password, PDO::PARAM_STR);
-        $stmt->bindParam(":2fa_code", $user_secret, PDO::PARAM_STR);
-
-        if ($stmt->execute()) {
-            // qrcode je premenna, ktora sa vykresli vo formulari v HTML.
-            $qrcode = $codeURL;
-        } else {
-            echo "Ups. Nieco sa pokazilo";
-        }
-
-        unset($stmt);
-    }
-    unset($pdo);
 }
 
-
 function checkEmpty($field) {
-    // Funkcia pre kontrolu, ci je premenna po orezani bielych znakov prazdna.
-    // Metoda trim() oreze a odstrani medzery, tabulatory a ine "whitespaces".
     if (empty(trim($field))) {
         return true;
     }
@@ -76,8 +31,6 @@ function checkEmpty($field) {
 }
 
 function checkLength($field, $min, $max) {
-    // Funkcia, ktora skontroluje, ci je dlzka retazca v ramci "min" a "max".
-    // Pouzitie napr. pre "login" alebo "password" aby mali pozadovany pocet znakov.
     $string = trim($field);
     $length = strlen($string);
     if ($length < $min || $length > $max) {
@@ -87,11 +40,19 @@ function checkLength($field, $min, $max) {
 }
 
 function checkStuba($email) {
-    // Funkcia pre kontrolu, ci zadany email je stuba
     if (!preg_match('/^[\w.+\-]+@stuba\.sk$/', trim($email))) {
         return false;
     }
     return true;
+}
+
+function checkPassword($password, $password2){
+    $pattern = "/^[a-zA-Z0-9]{8,20}$/";
+
+    if (preg_match($pattern, $password) && $password === $password2){
+        return true;
+    }
+    return false;
 }
 
 function userExist($db, $email, $userType) {
@@ -117,6 +78,23 @@ function userExist($db, $email, $userType) {
     unset($stmt);
     return $exist;
 }
-
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (!empty($errmsg)) {
+            // Tu vypis chybne vyplnene polia formulara.
+            echo $errmsg;
+        }
+    ?>
+</body>
+</html>
