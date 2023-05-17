4a1069c77e66428e5eface283e7ea568277c522d
Natalia Palenikova <xpalenikovan@stuba.sk>
2023-05-18 00:26:26 +0200

    index update

--- index.php
+++ index.php
@@ -1,39 +1,60 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    $displayValue = 'block';
+    $div = 'none';
+    if($_SESSION["userType"] == 'student'){
+        header("location: ../student/stefanov.php");
+    }
+}
+else{
+    $displayValue = 'none';
+    $div = 'block';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login and Signup</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav align-items-center ms-auto">
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="index_sk.php">
+                            <img src="resources/images/sk.png" alt="sk-flag" width="40" height="27">
+                        </a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#">
+                            <img src="resources/images/uk.png" alt="uk-flag" width="40" height="20">
+                        </a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="btn btn-secondary" href="../logout.php" style="display: <?php echo $displayValue; ?>">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
 <body>
-
-    <div id="flags">
-        <i class="flag flag-slovakia"></i>
-        <i class="flag flag-united-kingdom"></i>
+    <div class="container my-5" style="display: <?php echo $div; ?>">
+        <a class="btn btn-primary" href="login/login.php" role="button" style="background-color: #1261A0; border-color:#1261A0;">Login</a>
+        <a class="btn btn-primary" href="signup/signup.php" role="button" style="background-color: #072F5F; border-color:#072F5F;">Sign up</a>
     </div>
-    <?php
-        session_start();
-
-        if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-            echo 
-            '<div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <a class="btn btn-secondary" href="logout.php" role="button" style="position: absolute; top: 0; right: 0; margin: 10px;">Log out</a>
-                    </div>
-                </div>
-            </div>'; 
-        }
-        else{
-            echo
-            '<div class="container my-5">
-                <a class="btn btn-primary" href="login/login.php" role="button" style="background-color: #1261A0; border-color:#1261A0;">Login</a>
-                <a class="btn btn-primary" href="signup/signup.php" role="button" style="background-color: #072F5F; border-color:#072F5F;">Sign up</a>
-            </div>';
-        }
-    ?>
 </body>
 </html>
--- teacher/students_sk.php
+++ teacher/students_sk.php
@@ -36,7 +36,8 @@ $problems = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <!DOCTYPE html>
 <html>
-<head>
+<head> 
+    <title>Študenti</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap5.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
